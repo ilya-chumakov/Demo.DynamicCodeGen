7f681b1af4dea0d9a509172039e2ed45c7e4bf4f
Chumakov Ilya <aidforsoft@mail.ru>
2016-08-20 10:00:29 +0300

    Tests reorganized.

---
+++ Demo.DynamicCodeGen.Tests/Mappers_Tests.cs
@@ -0,0 +1,50 @@
+﻿using Demo.DynamicCodeGen.Common;
+using Demo.DynamicCodeGen.Emit;
+using Demo.DynamicCodeGen.ExpressionTrees;
+using Demo.DynamicCodeGen.ExternalMappers;
+using Demo.DynamicCodeGen.Roslyn;
+using NUnit.Framework;
+
+namespace Demo.DynamicCodeGen.Tests
+{
+    public class Mappers_Tests
+    {
+        [Test]
+        public void MyEmitMapper_Map_Success()
+        {
+            Test(MyEmitMapper.Instance);
+        }
+        
+        [Test]
+        public void MyExpressionMapper_Map_Success()
+        {
+            Test(MyExpressionMapper.Instance);
+        }
+        
+        [Test]
+        public void MyRoslynMapper_Map_Success()
+        {
+            Test(MyRoslynMapper.Instance);
+        }
+
+        [Test]
+        public void AutoMapperFacade_Map_Success()
+        {
+            Test(AutoMapperFacade.Instance);
+        }
+
+        private static void Test(ITestableMapper mapper)
+        {
+            var map = mapper.CreateMapMethod<Src, Dest>();
+
+            var src = new Src();
+            var dest = new Dest();
+
+            map(src, dest);
+
+            var result = ObjectComparer.AreEqual(src, dest);
+
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
